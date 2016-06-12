e1e9b0b04404cce6ab37d1cc2ab9b943b0f58a64
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-06-12 11:12:06 +0200

    LA-18 Add swype to delete even

--- src/LocationAlarm/Controls/AlarmItem/AlarmItemControl.xaml.cs
+++ src/LocationAlarm/Controls/AlarmItem/AlarmItemControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -6,9 +9,47 @@ namespace LocationAlarm.Controls.AlarmItem
 {
     public sealed partial class AlarmItemControl : UserControl
     {
+        private Point _endPosition;
+
+        private double _manipulationDeltaXThreshold = 340d;
+
+        private bool _manipulationStarted;
+        private Point _startPosition;
+
+        public event EventHandler SwypeToDeleteCompleted;
+
         public AlarmItemControl()
         {
             InitializeComponent();
+        }
+
+        private void AlarmItemControl_OnManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (!_manipulationStarted) return;
+
+            _manipulationStarted = false;
+            _endPosition = e.Position;
+        }
+
+        private void AlarmItemControl_OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (!_manipulationStarted) return;
+            var delta = e.Delta;
+            if (delta.Translation.X >= _manipulationDeltaXThreshold)
+                OnSwypeToDeleteCompleted();
+        }
+
+        private void AlarmItemControl_OnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            if (_manipulationStarted) return;
+
+            _manipulationStarted = true;
+            _startPosition = e.Position;
+        }
+
+        private void OnSwypeToDeleteCompleted()
+        {
+            SwypeToDeleteCompleted?.Invoke(this, EventArgs.Empty);
         }
     }
 }
